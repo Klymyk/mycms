f8e248c14f2425af12fb5c62263f6ec14e0c57c8
yura <klymyk@ukr.net>
2016-12-10 21:14:43 +0200

    first commit
    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XTreme.ws
+ * Date: 10.12.2016
+ * Time: 20:29
+ */
+
---
+++ module/post.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XTreme.ws
+ * Date: 10.12.2016
+ * Time: 20:23
+ */
+
+class Post
+{
+    /**
+     *
+     */
+    public function setPost ($tile,$content,$date)
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function getPost($id)
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function getAllPost ()
+    {
+
+    }
+
+    public function EditPost ($id,$tile,$content,$date)
+    {
+
+    }
+
+    public function DeletePost ($id)
+    {
+
+    }
+}
